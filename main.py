c8d488a5ad7a4499d434898384468e054a51d4e1
Your Name <you@example.com>
2020-11-23 11:00:22 +0100

    Canvis script

--- main.py
+++ main.py
@@ -22,26 +22,26 @@ if __name__ == '__main__':
 
         if(prueba==1):
                 archivo_input=input("Introduce el directorio del vídeo");
-                archivo_output=input("Introduce el directorio donde quieras que guarde el archivo y su nombre");
-                os.system("ffmpeg -ss 00:01:00.0 -i " +archivo_input+ " -c copy -t 00:00:05.0 1"+archivo_output+"");
+                archivo_output=input("Introduce el directorio donde quieras que guarde el archivo y su nombre")
+                os.system("ffmpeg -ss 00:01:00.0 -i " +archivo_input+ " -c copy -t 00:00:05.0 "+archivo_output+"");
         if(prueba==2):
                 archivo_input = input("Introduce el directorio del vídeo");
-                archivo_output = input("Introduce el directorio donde quieras que guarde el archivo y su nombre");
+                archivo_output = input("Introduce el directorio donde quieras que guarde el archivo y su nombre")
                 os.system("ffmpeg -i " +archivo_input+ " -vf split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay " +archivo_output+"");
         if(prueba==3):
                 archivo_input = input("Introduce el directorio del vídeo");
-                archivo_output = input("Introduce el directorio donde quieras que guarde el archivo y su nombre");
+                archivo_output = input("Introduce el directorio donde quieras que guarde el archivo y su nombre")
                 resolucion = input("Introduce la nueva resolución en formato Numero-Numero");
                 os.system("ffmpeg -i " + archivo_input + " -filter:v scale="+resolucion+" -acodec copy " + archivo_output + "");
         if(prueba==4):
                 archivo_input = input("Introduce el directorio del vídeo");
-                archivo_output = input("Introduce el directorio donde quieras que guarde el archivo y su nombre");
+                archivo_output = input("Introduce el directorio donde quieras que guarde el archivo y su nombre")
                 os.system("ffmpeg -i " +archivo_input+ " -ac 1 " +archivo_output+ "");
         if(prueba>4):
-                print("Opción no válida");
+                print("Opción no válida")
 
 
-        
+
 
 
         # See PyCharm help at https://www.jetbrains.com/help/pycharm/
